e313d5720d90f79e02b2b3a6eec705ab085fa9fa
vishal <yalamelivishal123@gmail.com>
2025-03-02 22:37:57 +0530

    new file added

---
+++ DailyCodes/Control_Statement/if/ifDemo.java
@@ -0,0 +1,15 @@
+class ifDemo{
+	public static void main(String[] args){
+		int x= 10;
+		int y= 9;
+                   
+		System.out.println("Statement 1");
+		if(x != y){
+			System.out.println("Ststement 2");
+		}
+		System.out.println("Statement3");
+	}
+}
+// o/p: Statement 1
+// statement 2 
+// statement 3 
