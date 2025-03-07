6670fe1c26fea23384fd1d46b4fe6a7e63a28f93
vishal <yalamelivishal123@gmail.com>
2025-03-08 00:56:04 +0530

    New code added

---
+++ DailyCodes/Control_Statement/ifElse/Program2.java
@@ -0,0 +1,14 @@
+class Program2{
+	public static void main(String [] args){
+		boolean x = false;
+		boolean y = true;
+		System.out.println("start code");
+		if( x =  y ){
+			System.out.println("In if");
+		}else{
+			System.out.println("Ende code");
+		}
+	
+	
+}
+}
