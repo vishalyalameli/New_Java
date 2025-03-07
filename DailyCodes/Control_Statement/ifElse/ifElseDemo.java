c68ec1cf092c7df9434c4a1c877e2dcca729599a
vishal <yalamelivishal123@gmail.com>
2025-03-07 22:29:28 +0530

    new code added

---
+++ DailyCodes/Control_Statement/ifElse/ifElseDemo.java
@@ -0,0 +1,13 @@
+class ifElseDemo{
+	public static void main (String [] args){
+		int x = 10;
+		int y = 11;
+		System.out.println("start code");
+		if( x!= y){
+			System.out.println("in if");
+		}else{
+			System.out.println("In If");
+		}
+		System.out.println("End code");
+	}
+}
