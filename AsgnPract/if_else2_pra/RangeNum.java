e1add9745e146b7490589e32d9d58cd6662934ee
vishal <yalamelivishal123@gmail.com>
2025-03-13 23:41:01 +0530

    New code Added practicle

---
+++ AsgnPract/if_else2_pra/RangeNum.java
@@ -0,0 +1,12 @@
+class RangeNum{
+	public static void main(String [] args){
+
+		int num = -11; // 0 ,  100
+
+		if( num >= 1 &&  num <=1000){
+			System.out.println(num +"  in the rang "+ 1+ "to "+1000);
+		}else{
+			System.out.println(num +" is not the range" +1+ "to" +1000);
+		}
+	}
+}
---
+++ AsgnPract/if_else2_pra/TableNum.java
@@ -0,0 +1,11 @@
+class TableNum{
+	public static void main(String [] args){
+		int num =144; // 156
+
+		if( num % 13 == 0){
+			System.out.println(num + "  in table of" +13);
+		} else{
+			System.out.println(num + " is not in the table" + 13);
+		}
+	}
+}
