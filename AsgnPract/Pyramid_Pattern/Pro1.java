af498f63775e551026277086dda7d95c578dcdfb
vishal <yalamelivishal123@gmail.com>
2025-04-22 00:11:45 +0530

    Ini

--- AsgnPract/Pyramid_Pattern/Pro1.java
+++ AsgnPract/Pyramid_Pattern/Pro1.java
@@ -1,26 +1,32 @@
-
 import java.util.*;
 
- class Pro1 {
-    public  static void main(String [] args){
-        Scanner  sc = new Scanner(System.in);
+class Pro1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter Number");
 
         int rows = sc.nextInt();
 
-        for(int i = 1; i<=rows; i++){
-            for(int s = 1 ; s <=rows -i; s++){
-                System.out.println("\t");
+        for (int i = 1; i<= rows; i++) {
+            for (int s = 1; s <= rows - i; s++) {
+                System.out.print(" \t");
             }
-               for(int j = 1 ; j<= (2 * i -1); j++ ){
-                System.out.println((rows-i+1) + "\t");
-               }
+            for (int j = 1; j <= (2 * i - 1); j++) {
 
-               System.out.println();
+                System.out.print((rows - i + 1) + " \t");
             }
-       
 
+            System.out.println();
+        }
 
     }
-    
+
 }
+
+/*
+ Enter Number
+3
+                3 
+        2       2       2
+1       1       1       1       1
+ */
