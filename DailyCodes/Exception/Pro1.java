c4042a5b42b2dc08665427a62685153ca50b5bf5
vishal <yalamelivishal123@gmail.com>
2025-07-10 19:45:35 +0530

    exception

---
+++ DailyCodes/Exception/Pro1.java
@@ -0,0 +1,9 @@
+ class Pro1 {
+    public static void main(String [] args){
+        int arr[]  = {10,20,30,40,50};
+        System.out.println(arr[3]);
+        System.out.println(arr[5]);
+    }
+    
+}
+
