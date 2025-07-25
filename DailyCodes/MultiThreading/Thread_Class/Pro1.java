f8d47786d6aff169a5533773d17dff76a4cd9d5d
vishal <yalamelivishal123@gmail.com>
2025-07-25 11:38:05 +0530

    Multi_ Threading

---
+++ DailyCodes/MultiThreading/Thread_Class/Pro1.java
@@ -0,0 +1,9 @@
+class client{
+	public static void main(String [] args){
+		System.out.println("Start Main");
+		System.out.println(Thread.currentThread().getName());
+		System.out.println("End Main");
+	}
+}
+
+
