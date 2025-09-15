8f5b527d312b3d3cb0e4b8c5dccef46d7b29150c
vishal <yalamelivishal123@gmail.com>
2025-09-15 17:19:18 +0530

    Collection Queue

--- DailyCodes/collections/Queue/pro1.java
+++ DailyCodes/collections/Queue/pro1.java
@@ -13,7 +13,28 @@ class QueueMethods{
         queue.offer("rahul");
         System.out.println(queue);
 
-        
+        //peek
+        System.out.println(queue.peek());// if empty null
+
+        //element
+        System.out.println(queue.element()); // if empty- Exception
+
+        //poll
+        System.out.println(queue.remove()); // if empty -excetion
+
+        System.out.println(queue);
+
+
+        //size
+        System.out.println(queue.size());// 2
+
+        //contains
+        System.out.println(queue.contains("kanha")); // false
+
+        //clear
+        queue.clear();
+        System.out.println(queue);
+
 
 
     }
